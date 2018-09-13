7071c2b866ba68f2ed14720aa8f8e174a21846b0
Rashid <abdirashidjama@cmail.carleton.ca>
2018-09-13 17:03:58 -0400

    * added testbetterHand to compare hands of dealer and player

--- src/test/java/blackjack/CardCompareTest.java
+++ src/test/java/blackjack/CardCompareTest.java
@@ -1,13 +1,32 @@
 package blackjack;
 
 import junit.framework.TestCase;
+import java.util.*;
 
 
 public class CardCompareTest extends TestCase {
 	public void testCheckBust() {
 		Game solver = new Game();
-		String[] cards =  {"H9","AK", "S6"} 
+		ArrayList<String> cards = new ArrayList<String>() {{
+			add("H9");
+			add("AK");
+			add("S6");
+				
+		}};
 		assertEquals(true, solver.checkBust(cards));
+	}
+	
+	public void testBetterHand() {
+		Game solver = new Game();
+		ArrayList<String> playerhand = new ArrayList<String>() {{
+			add("H8");
+			add("AK");
+		}};
+		ArrayList<String> dealerhand = new ArrayList<String>() {{
+			add("D9");
+			add("HJ");
+		}};
+		assertEquals(true, solver.betterHand(playerhand, dealerhand));
 	}
 
 }
