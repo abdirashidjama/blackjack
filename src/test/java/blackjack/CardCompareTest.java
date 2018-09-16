c23e762a5693b535123278bfaf0352e65be9e4e4
Rashid <abdirashidjama@cmail.carleton.ca>
2018-09-16 10:19:10 -0400

    *added testPlayerCards() which tests initial display for player hands

--- src/test/java/blackjack/CardCompareTest.java
+++ src/test/java/blackjack/CardCompareTest.java
@@ -30,12 +30,40 @@ public class CardCompareTest extends TestCase {
 		solver.hit();
 		assertEquals(14, solver.handValue());	
 	}
-	
-	public void teststand(){
+	public void testdealerMultihit(){
 		Game solver = new Game();
 		Stack deck = new Stack();
 		deck.push("A2");
+		deck.push("A3");
+		deck.push("A4");
+		deck.push("A5");
 		solver.setDeck(deck);
+		solver.stand();
+		solver.hit();
+		solver.hit();
+		solver.hit();
+		solver.hit();
+		assertEquals(14, solver.handValue());	
+	}
+	
+	public void teststand() {
+		Game solver = new Game();
+		Stack deck = new Stack();
+		deck.push("A2");
+		deck.push("A3");
+		deck.push("A4");
+		deck.push("A5");
+		solver.setDeck(deck);
+		solver.setHands('p');
+		solver.setHands('d');
+		solver.stand();
+		assertEquals("dealer", solver.getcurrentTurn()); //turn should change to dealer after stand
+	}
+	
+	public void test52cards(){
+		Game solver = new Game();
+		solver.start('c');
+		assertEquals(52, solver.getDeck().size()); //testing list to make sure it has 52 cards
 	}
 	
 	public void testCheckBust() {
@@ -50,6 +78,19 @@ public class CardCompareTest extends TestCase {
 		solver.hit();
 		assertEquals(null, solver.handValue()); // bust hands are null
 	}
+	
+	public void testplayerCards() {
+		Game solver = new Game();
+		Stack deck = new Stack();
+		deck.push("A2");
+		deck.push("A3");
+		deck.push("A4");
+		deck.push("A5");
+		solver.setCards('p');
+		assertEquals("A5 A4", solver.displayPlayerHand());
+	}
+	
+	
 	
 	public void testBetterHand() { //sees if dealer has a better hand then player
 		Game solver = new Game();
@@ -86,10 +127,6 @@ public class CardCompareTest extends TestCase {
 		assertEquals(19,solver.handValue(cards)); //regular hand test
 	}
 	
-	public void test52cards(){
-		Game solver = new Game();
-		assertEquals(52, solver.getDeck().size()); //testing list to make sure it has 52 cards
-	}
 	
 	public void testcurrentTurn(){ //test if stand function changes whose turn it is
 		Game solver = new Game();
